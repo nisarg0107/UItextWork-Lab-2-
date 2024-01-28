a99e802be403c4b2ebe330aacd3aa41ed7945454
nisarg0107 <nisarg172001@gmail.com>
2024-01-28 18:47:39 -0500

    first committee

--- Assignment2/ViewController.swift
+++ Assignment2/ViewController.swift
@@ -9,6 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+ 
+    @IBOutlet weak var fname: UITextField!
+    
+    @IBOutlet weak var lname: UITextField!
+    
+    @IBOutlet weak var country: UITextField!
+    
+    @IBOutlet weak var age: UITextField!
+    
+    @IBOutlet weak var displayArea: UITextView!
+    
+    @IBOutlet weak var responseArea: UILabel!
+    
+    
+    @IBAction func addInfo(_ sender: Any) {
+        displayArea.text = "Full Name : \(fname.text!) \(lname.text!) \nCountry : \(country.text!) \nAge : \(age.text!)"
+
+    }
+    
+    
+    @IBAction func submitInfo(_ sender: Any) {
+        
+        if (fname.text!.isEmpty || lname.text!.isEmpty || country.text!.isEmpty || age.text!.isEmpty){
+          
+            responseArea.text = "Complete the missing Info!"
+        }
+        else{
+            
+           
+            responseArea.text = "Successfully submitted!"
+            displayArea.text = "Full Name : \(fname.text!) \(lname.text!) \nCountry : \(country.text!) \nAge : \(age.text!)"
+        }
+    }
+    
+    
+    @IBAction func clearInfo(_ sender: Any) {
+        fname.text!.removeAll()
+        lname.text!.removeAll()
+        country.text!.removeAll()
+        age.text!.removeAll()
+        displayArea.text!.removeAll()
+        responseArea.text!.removeAll()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
